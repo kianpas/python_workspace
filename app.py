ded4871996f50d39ad99c8f93a590674477c5bb2
kianpas <kianpas@gmail.com>
2022-01-19 21:55:03 +0900

    22.01.19 메인 함수 설정

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from multiapp import MultiApp
 import data_test
 import url_test
 
-# MultiApp
-app = MultiApp()
+def main():
+    # MultiApp
+    app = MultiApp()
 
-app.add_app("데이터 수집", data_test.app)
-app.add_app("URL", url_test.app)
+    app.add_app("데이터 수집", data_test.app)
+    app.add_app("URL", url_test.app)
 
-# MultiApp의 run 실행
-app.run()
+    # MultiApp의 run 실행
+    app.run()
+
+if __name__ == "__main__":
+    main()
